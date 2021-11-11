58854c5310a9aa1dc1036db901b79102969d43cb
Juan Pablo Genaro <jpgenaro06@gmail.com>
2021-11-11 12:01:14 -0300

    osi osi

--- Juegos/TAC/assets/main.js
+++ Juegos/TAC/assets/main.js
@@ -1,7 +1,22 @@
-let modulo = require('./Pais');
-let computador = new modulo.Pais("Argentina");
+// CREACION DE PAISES ===========================================================
+class Pais {
+    constructor (nombre) {
+      this.nombre = nombre;
+    }
 
-// Cargar datos del usuario
-function jugar(){
-    console.log(computador.getDatos());
+    get Datos() {
+      return this.nombre;
+    }
+}
+// const pais_1 = new Pais("Argentina");
+
+function creacion(){
+    var name = document.getElementById("apodo").value;
+    if (name != "") {
+        var eleccion = document.getElementById("comboBox");
+        var selected = eleccion.options[eleccion.selectedIndex].text;
+        window.alert(selected);
+    }else {
+        window.alert("Complete los campos restantes!");
+    }
 }
